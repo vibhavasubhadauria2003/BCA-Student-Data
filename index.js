656c3563651260fe20cce5b9fe1db7d6dc57c735
Vibhavasu Bhadauria <138148506+vibhavasubhadauria2003@users.noreply.github.com>
2024-10-09 13:53:45 +0530

    version 6 [(01:53 PM)(09/10/2024)]

--- index.js
+++ index.js
@@ -8,22 +8,33 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 
-app.get("/Rohit_Sharma",(req,res)=>{
-    res.sendFile(path.join(__dirname, 'Front-END/index.html'));
-})
-
 app.set('view engine', 'ejs');
+app.get('/profile/:n',(req,res)=>{
+    res.send(`Value you send to page is ${req.params.n}`);
+})
 
 app.get("/Vibhavasu",(req,res)=>{
     res.render('index',{
         profile_link:'vibhavasu.jpg',
-        about:'Rohit Gurunath Sharma is an Indian international cricketer who currently plays for and captains the India national cricket team in Test and One Day International matches.',
+        about:'Vibahvasu Bhadauria Student of DSVV. Currently persuing BCA.',
         user_name:'Vibhavasu Bhadauria',
         profession:'Student',
         dob:'13 April 2003',
         father_name:'Prabodh Kumar Bhadauria',
         mother_name:'Seema Bhadauria',
         user_location:'Mainpuri'
+    });
+})
+app.get("/Rohit_Sharma",(req,res)=>{
+    res.render('index',{
+        profile_link:'rohit-sharma.WEBP',
+        about:'Rohit Gurunath Sharma is an Indian international cricketer who currently plays for and captains the India national cricket team in Test and One Day International matches.',
+        user_name:'Rohit Sharma',
+        profession:'Indian Cricketer',
+        dob:'30 April 1987',
+        father_name:'Gurunath Sharma',
+        mother_name:'Purnima Sharma',
+        user_location:'Mumbai'
     });
 })
 
