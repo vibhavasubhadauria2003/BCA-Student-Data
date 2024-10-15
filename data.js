d78ddf9b9f455400e48502169643a70a89aa6362
Vibhavasu Bhadauria <138148506+vibhavasubhadauria2003@users.noreply.github.com>
2024-10-15 14:52:21 +0530

    version 9 [(02:52 PM)(15/10/2024)]

---
+++ data.js
@@ -0,0 +1,94 @@
+class UserData{
+    constructor(profile_link,about,user_name,profession,dob,father_name,mother_name,user_location){
+        this.profile_link = profile_link;
+        this.about=about;
+        this.user_name=user_name;
+        this.profession=profession;
+        this.dob=dob;
+        this.father_name=father_name;
+        this.mother_name=mother_name;
+        this.user_location=user_location;
+    }
+}
+
+export const vibhavasu=new UserData(
+    '/images/vibhavasu.jpg',
+    'Vibahvasu Bhadauria Student of DSVV. Currently persuing BCA.',
+    'Vibhavasu Bhadauria',
+    'Student',
+    '13 April 2003',
+    'Prabodh Kumar Bhadauria',
+    'Seema Bhadauria',
+    'Mainpuri'
+)
+
+export const rohit_sharma=new UserData(
+    '/images/rohit-sharma.WEBP',
+    'Rohit Gurunath Sharma is an Indian international cricketer who currently plays for and captains the India national cricket team in Test and One Day International matches.',
+    'Rohit Sharma',
+    'Indian Cricketer',
+    '30 April 1987',
+    'Gurunath Sharma',
+    'Purnima Sharma',
+    'Mumbai'
+)
+export const sarthak=new UserData(
+    '/images/sarthak.jpg',
+    'Sarthak Shukla has a unique personality, I can sleep whole day and night.',
+    'Sarthak Shukla',
+    'Student',
+    '03 June 2003',
+    'Shivakant Shukla',
+    'Kalpna Shukla',
+    'Prayagraj'
+)
+export const abhinav=new UserData(
+    '/images/vibhavasu.jpg',
+    'Vibahvasu Bhadauria Student of DSVV. Currently persuing BCA.',
+    'Vibhavasu Bhadauria',
+    'Student',
+    '13 April 2003',
+    'Prabodh Kumar Bhadauria',
+    'Seema Bhadauria',
+    'Mainpuri'
+)
+export const anshuman=new UserData(
+    '/images/vibhavasu.jpg',
+    'Vibahvasu Bhadauria Student of DSVV. Currently persuing BCA.',
+    'Vibhavasu Bhadauria',
+    'Student',
+    '13 April 2003',
+    'Prabodh Kumar Bhadauria',
+    'Seema Bhadauria',
+    'Mainpuri'
+)
+export const tarun=new UserData(
+    '/images/vibhavasu.jpg',
+    'Vibahvasu Bhadauria Student of DSVV. Currently persuing BCA.',
+    'Vibhavasu Bhadauria',
+    'Student',
+    '13 April 2003',
+    'Prabodh Kumar Bhadauria',
+    'Seema Bhadauria',
+    'Mainpuri'
+)
+export const anupam=new UserData(
+    '/images/vibhavasu.jpg',
+    'Vibahvasu Bhadauria Student of DSVV. Currently persuing BCA.',
+    'Vibhavasu Bhadauria',
+    'Student',
+    '13 April 2003',
+    'Prabodh Kumar Bhadauria',
+    'Seema Bhadauria',
+    'Mainpuri'
+)
+export const akhil=new UserData(
+    '/images/akhil.jpg',
+    'Akhil Pratap Singh Student of DSVV. Currently persuing BCA.',
+    'Akhil Pratap Singh',
+    'Student',
+    '21 August 2005',
+    'Abnish Kumar',
+    'Kirti Singh',
+    'Auraiya'
+)
--- index.js
+++ index.js
@@ -1,54 +1,41 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { app } from "./app.js";
+import { vibhavasu,akhil,rohit_sharma,sarthak } from './data.js';
+
 const port = 7000
 
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 
 app.set('view engine', 'ejs');
-app.get('/profile/:n',(req,res)=>{
-    res.send(`Value you send to page is ${req.params.n}`);
-})
+// app.get('/profile/:n',(req,res)=>{
+//     res.send(`Value you send to page is ${req.params.n}`);
+// })
 
-app.get("/Vibhavasu",(req,res)=>{
-    res.render('index',{
-        profile_link:'/images/vibhavasu.jpg',
-        about:'Vibahvasu Bhadauria Student of DSVV. Currently persuing BCA.',
-        user_name:'Vibhavasu Bhadauria',
-        profession:'Student',
-        dob:'13 April 2003',
-        father_name:'Prabodh Kumar Bhadauria',
-        mother_name:'Seema Bhadauria',
-        user_location:'Mainpuri'
-    });
-})
-app.get("/Rohit_Sharma",(req,res)=>{
-    res.render('index',{
-        profile_link:'/images/rohit-sharma.WEBP',
-        about:'Rohit Gurunath Sharma is an Indian international cricketer who currently plays for and captains the India national cricket team in Test and One Day International matches.',
-        user_name:'Rohit Sharma',
-        profession:'Indian Cricketer',
-        dob:'30 April 1987',
-        father_name:'Gurunath Sharma',
-        mother_name:'Purnima Sharma',
-        user_location:'Mumbai'
-    });
-})
-app.get("/Akhil",(req,res)=>{
-    res.render('index',{
-        profile_link:'/images/akhil.jpg',
-        about:'Akhil Pratap Singh Student of DSVV. Currently persuing BCA.',
-        user_name:'Akhil Pratap Singh',
-        profession:'Student',
-        dob:'21 August 2005',
-        father_name:'Abnish Kumar',
-        mother_name:'Kirti Singh',
-        user_location:'Auraiya'
-    });
-})
+function serve(name,data){
+    app.get(name,(req,res)=>{
+        res.render('index',data);
+    })
+}
+
+// app.get("/Vibhavasu",(req,res)=>{
+//     res.render('index',vibhavasu);
+// })
+// app.get("/Rohit_Sharma",(req,res)=>{
+//     res.render('index',rohit_sharma);
+// })
+// app.get("/Akhil",(req,res)=>{
+//     res.render('index',akhil);
+// })
+
+serve("/Akhil",akhil);
+serve("/Vibhavasu",vibhavasu);
+serve("/Rohit-Sharma",rohit_sharma);
+serve("/Sarthak",sarthak)
+
 
 app.listen(port,()=>{
     console.log(`Server runing at ${port}`)
